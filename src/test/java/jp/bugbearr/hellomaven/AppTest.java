b0be2fde9c5420f9c53f60a401a61faade49ca08
BugbearR <bugbearr@bugbearr.jp>
2020-06-05 16:12:42 +0900

    Initial

---
+++ src/test/java/jp/bugbearr/hellomaven/AppTest.java
@@ -0,0 +1,20 @@
+package jp.bugbearr.hellomaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
